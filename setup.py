22fa15810d8c4e64786fb0c863d47e626f72ebcf
csae8092 <peter.andorfer@oeaw.ac.at>
2020-11-23 21:03:22 +0100

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/acdh-oeaw/freud_api_crawler',
-    version='0.8.0',
+    version='0.9.0',
     zip_safe=False,
 )
