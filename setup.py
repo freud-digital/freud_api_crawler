b67d67558ff2226f9acf3c942eb48dc1e0d1aa1d
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2020-12-09 21:00:56 +0100

    minor

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
             'freud_api_crawler/fixtures',
             [
                 'freud_api_crawler/fixtures/tei_dummy.xml',
-                'freud_api_crawler/fixtures/make_tei.xsl',
+                'freud_api_crawler/fixtures/make_tei.xslt',
             ]
         ),
     ],
